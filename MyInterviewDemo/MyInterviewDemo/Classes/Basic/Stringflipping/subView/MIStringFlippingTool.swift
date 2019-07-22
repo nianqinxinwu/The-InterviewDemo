fe65bc8d14059120809d335afcc59e2608bb7440
常远 <changyuan@yingu.com>
2019-07-22 17:02:29 +0800

    字符串交换

--- MyInterviewDemo/MyInterviewDemo/Classes/Basic/Stringflipping/subView/MIStringFlippingTool.swift
+++ MyInterviewDemo/MyInterviewDemo/Classes/Basic/Stringflipping/subView/MIStringFlippingTool.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MIStringFlippingTool: NSObject {
     
+    /*******swift4.0 + ************/
     func rever(s:String) -> String? {
         return String(s.reversed())
     }
@@ -37,6 +38,45 @@ class MIStringFlippingTool: NSObject {
         
         return String(result)
     }
+    
+    /* Swift 4.0 -*/
+    
+//    fileprivate func swap<T>(_ chars : inout [T], _ p : Int, _ q:Int){
+//        print("before p:",chars[p],"q:",chars[q])
+//        (chars[p],chars[q]) = (chars[q],chars[p])
+//        print("after p:",chars[p],"q:",chars[q])
+//    }
+//
+//    fileprivate func _reverse<T>(_ chars: inout [T], _ start : Int, _ end : Int){
+//        var start = start, end = end
+//
+//        while start < end {
+//            swap(&chars, start, end)
+//            start += 1
+//            end -= 1
+//        }
+//    }
+//
+//    func reverseWords(s:String) -> String? {
+//        NSLog("oldWord:%@\n", s)
+//        guard let s:String = s else {
+//            return nil
+//        }
+//        var chars = Array(s.characters),start = 0
+//        //整体反转
+//        _reverse(&chars, 0, chars.count - 1)
+//
+//        //以“ ”和字符串结合中最后一个字母为界对单个单词进行反转
+//        for i in 0..<chars.count {
+//            if i == chars.count - 1 || chars[i + 1] == " " {
+//                _reverse(&chars, start, i)
+//                start = i + 2
+//            }
+//        }
+//
+//        return String(chars)
+//    }
+    
     /*
      字符串反转中描述很清晰的地址:https://www.jianshu.com/p/cb3ed74c0732
      */
--- MyInterviewDemo/MyInterviewDemo/RootClass/ViewController.swift
+++ MyInterviewDemo/MyInterviewDemo/RootClass/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tool = MIStringFlippingTool.init()
+        let testStr = tool.reverseWords(s: "new word should be speaking")
+        print("reslut:",testStr!)
     }
 
 
